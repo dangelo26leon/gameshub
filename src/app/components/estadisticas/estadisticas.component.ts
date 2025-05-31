dc1b8390af78a7419a2fb6436e250efa09d591f9
Dangelo26leon <leonvalderramadangeloalexander@gmail.com>
2025-05-30 19:20:35 -0500

    component estadistica

--- src/app/components/estadisticas/estadisticas.component.ts
+++ src/app/components/estadisticas/estadisticas.component.ts
@@ -13,16 +13,53 @@ RESPUESTAS PARTE 4.2:
 1. ¿Por qué este proyecto No tiene app.module.ts?
 Porque este proyecto al ser creado se puso en --standalone, eso hace que no se use NgModules y se usa app.config.ts en su lugar.
 2. ¿Qué ventaja tiene usar BehaviorSubject en el servicio de juegos?
-Tiene como ventaja de siempre poner el estado actual del juego
+Tiene como ventaja de siempre poner el estado actual del juego a las personas.
 */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { JuegosDataService } from '../../services/juegos-data.service';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './estadisticas.component.html',
-  styleUrl: './estadisticas.component.css'
+  styleUrl: './estadisticas.component.css',
+  providers: [CurrencyPipe, DecimalPipe]
 })
-export class EstadisticasComponent {
+export class EstadisticasComponent implements OnInit {
+  juegosTotal: number = 0;
+  juegosGratis: number = 0;
+  juegosdePago: number = 0;
+  mejorRating: { nombre: string, rating: number } = { nombre: '', rating: 0 };
+  promPrecio: number = 0;
+
+  constructor (private juegosService: JuegosDataService){}
+
+  ngOnInit(): void {
+    this.juegosService.obtenerJuegos().subscribe(juegos => {
+      this.juegosTotal=juegos.length;
+
+      const juegosdePago = juegos.filter(juegos => juegos.precio>0);
+      const juegosGratis = juegos.filter(juegos => juegos.precio===0);
+
+      this.juegosdePago=juegosdePago.length;
+      this.juegosGratis=juegosGratis.length;
+
+      if(juegos.length>0){
+        const mejor=juegos.reduce((prev,curr)=>prev.rating > curr.rating ? prev: curr);
+        this.mejorRating={ nombre: mejor.nombre, rating: mejor.rating };
+      }
+
+      if(juegosdePago.length>0) {
+        const sumPrecio=juegosdePago.reduce((acc, juego) => acc + juego.precio, 0);
+        this.promPrecio=sumPrecio/juegosdePago.length;
+      }
+
+    })
+      
+  }
 
 }
